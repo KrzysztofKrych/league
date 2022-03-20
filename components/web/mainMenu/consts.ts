27ca2e7116905e56fe4515166867d94e4e2b124f
KrzysztofKrych <krzysztofkrych2@gmail.com>
2022-03-20 11:58:13 +0100

    change name

--- components/web/mainMenu/consts.ts
+++ components/web/mainMenu/consts.ts
@@ -1,4 +1,4 @@
 import { Route } from '../../../utils/enum';
 import { MainMenuItem } from './interfaces';
 
-export const MENU_ITEMS: MainMenuItem[] = [{ name: 'Your league', route: Route.DASHBOARD }, { name: 'Individual leaderboard', route: Route.INDIVIDUAL_LEADERBOARD }];
+export const MENU_ITEMS: MainMenuItem[] = [{ name: 'Dashboard', route: Route.DASHBOARD }, { name: 'Individual leaderboard', route: Route.INDIVIDUAL_LEADERBOARD }];
